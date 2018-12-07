d079da78353d8afd23f72c852b49d967457f73fd
Jeremy Sung <jsungcoding@gmail.com>
2018-12-06 23:47:56 -0800

    Added - Merged Retrieved Text Subtitles into one file in retrieved folder.

--- python/coding/merge_textfiles.py
+++ python/coding/merge_textfiles.py
@@ -21,21 +21,67 @@ class merge_textfiles:
       print("{} does not exist.".format(path))
       return
 
-    queue = deque()
-    for dir in os.listdir(path):
+    # queue = deque()
+    # for dir in os.listdir(path):
+    #
+    #   if "zz" in dir or dir.endswith(".zip"):
+    #     continue
+    #
+    #   sub = path + "/" + dir
+    #
+    #   if os.path.isdir(sub):
+    #     print(sub)
+    #
+    #   queue.append(sub)
 
-      if "zz" in dir or dir.endswith(".zip"):
-        continue
+    # queue = deque([path + "/" + dir for dir in os.listdir(path) if ("zz" not in dir or not dir.endswith(".zip")) and
+    #                os.path.isdir(path + "/" + dir)])
 
-      sub = path + "/" + dir
-
-      # if os.path.isdir(sub):
-      #   print(sub)
-
-      queue.append(sub)
+    queue = deque( path + "/" + dir for dir in os.listdir(path) if ("zz" not in dir or not dir.endswith(".zip")) and
+                   os.path.isdir(path + "/" + dir) )
 
     ## pp.pprint(queue)
+    ## print(queue.popleft())
 
+    # while queue:
+    #   subdir = queue.popleft()
+
+    for dir in queue:
+
+      for subdir in os.listdir(dir):
+
+        # mergedFileName = subdir + "_mergedSubtitles"
+
+        if "retrieved" in subdir and os.path.isdir(dir + '/' + subdir):
+
+          mergedFileName = dir + '/' + subdir + '/' + "mergedSubtitles.txt"
+
+          for src in os.listdir(dir + '/' + subdir):
+            srcFile = dir + '/' + subdir + '/' + src
+
+            with open(srcFile, "r") as srcf, open(mergedFileName, "a") as mergedf:
+
+              for text in filter(None, (line.rstrip() for line in srcf)):
+
+                if "-->" not in text and not text.isnumeric():
+                  mergedf.write(text + ' ')
+
+        # for srcFile in os.listdir(subdir):
+        #
+        #   if srcFile.endswith("lang_en.srt") or "lang_en" in srcFile:
+        #
+        #     # dstFile = dir + '/' + srcFile.strip(".srt") + r".txt"
+        #     srcFile = dir + '/' + subdir + '/' + srcFile
+        #
+        #     ## print("Source: {};\n Destination: {}".format(srcFile, dstFile))
+        #
+        #     # self.extractSubtitles(srcFile, dstFile)
+        #     with open(srcFile, "r") as srcf, open(mergedFileName, "a") as mergedf:
+        #
+        #       for text in filter(None, (line.rstrip() for line in srcf)):
+        #
+        #         if "-->" not in text and not text.isnumeric():
+        #           mergedf.write(text + ' ')
 
 
 
@@ -79,7 +125,8 @@ class merge_textfiles:
 
 if __name__ == "__main__":
   inst_merge_textfiles = merge_textfiles()
-  path = "C:/38-Git"
+  # path = "C:/38-Git"
+  path = "C:/zz_A1_Backup/38-Git"
   inst_merge_textfiles.merge_files(path)
   
   
